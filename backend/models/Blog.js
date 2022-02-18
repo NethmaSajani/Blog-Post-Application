c8eeb7c7bde34d8dc6898ff2356410900baa24bf
NethmaSajani <ynsajani@gmail.com>
2022-02-18 12:45:50 +0530

    Backend

---
+++ backend/models/Blog.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    Id:{
+        type:String,
+        required:true
+    },
+    Title:{
+        type:String,
+        required:true
+    },
+    Content:{
+        type:String,
+        required:true
+    },
+    Author:{
+        type:String,
+        required:true
+    },
+    CreatedDate:{
+        type:String,
+        required:true
+    }
+})
+
+const Blog = mongoose.model("Blog", BlogSchema);
+
+module.exports = Blog;
---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+const PORT = process.env.PORT || 8070;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL);
+
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB connection success!");
+});
+
+const blogRouter = require("../backend/routes/blogRoute");
+
+app.use("/blog", blogRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
