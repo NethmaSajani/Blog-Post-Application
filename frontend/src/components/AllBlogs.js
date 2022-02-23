a8f182b85950bdeb2b0b54f968dc0d30bc66d3c4
NethmaSajani <80049117+NethmaSajani@users.noreply.github.com>
2022-02-23 20:33:32 +0530

    Completed the project

--- frontend/src/components/AllBlogs.js
+++ frontend/src/components/AllBlogs.js
@@ -1,38 +1,32 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button, Card, Row, Col } from "react-bootstrap";
 import "../App.css";
+import "./Styles/blogs.css";
 
 const Blog = (props) => (
-  <div className="container" style={{ padding: "10px" }}>
-    <Row>
-      <Col>
-        <h5>{props.blog.Title}</h5>
-        <p id="content">{props.blog.Content}</p>
-        <p>{props.blog.createdAt}</p>
-        <br></br>
-      </Col>
-      <Col id="column">
-        <div>
-          <button
-            style={{ marginBottom: "10px" }}
-            className="btn btn-link"
-            onClick={() => {
-              props.deleteBlog(props.blog._id);
-            }}
-          >
-            Delete Blog
-          </button>
-        </div>
-        <div>
-          <button>
-            <Link className="btn btn-link" to={`/update/${props.blog._id}`}>
-              Edit Blog
-            </Link>
-          </button>
-        </div>
-      </Col>
-    </Row>
+  <div id="container1">
+    <h4 id="header">{props.blog.Title}</h4>
+    <br></br>
+    <p id="content">{props.blog.Content}</p>
+    <br></br>
+    <p id="date">{props.blog.createdAt}</p>
+    <br></br>
+
+    <div id="btn_content">
+      <Link className="update_btn" to={`/update/${props.blog._id}`}>
+        Edit Blog
+      </Link>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      <button
+        className="dlt_btn"
+        onClick={() => {
+          props.deleteBlog(props.blog._id);
+        }}
+      >
+        Delete Blog
+      </button>
+    </div>
+
     <hr></hr>
   </div>
 );
@@ -40,7 +34,7 @@ const Blog = (props) => (
 export default function AllBlogs() {
   const [blogs, setBlogs] = useState([]);
 
-  // This method fetches the records from the database
+  // This method fetches blogs from the database
   useEffect(() => {
     async function getBlogs() {
       const response = await fetch(`http://localhost:8070/blog/`);
@@ -83,7 +77,7 @@ export default function AllBlogs() {
     });
   }
 
-  // This following section will display all blogs
+  // This will display all blogs
   return (
     <div>
       <h1 id="title">ALL BLOGS</h1>
